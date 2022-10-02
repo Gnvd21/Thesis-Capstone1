94393214fb0a09cc687c1c74ceb1595f34117eac
Gnvd21 <giodagoro@gmail.com>
2022-10-03 00:12:52 +0800

    Python code and test image

---
+++ Colors.py
@@ -0,0 +1,159 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+import sklearn
+from sklearn.cluster import KMeans
+from collections import Counter
+from skimage.color import rgb2lab, deltaE_cie76
+import cv2
+plt.style.use('ggplot')
+plt.rcParams['font.family'] = 'sans-serif' 
+plt.rcParams['font.serif'] = 'Ubuntu' 
+plt.rcParams['font.monospace'] = 'Ubuntu Mono' 
+plt.rcParams['font.size'] = 14 
+plt.rcParams['axes.labelsize'] = 12 
+plt.rcParams['axes.labelweight'] = 'bold' 
+plt.rcParams['axes.titlesize'] = 12 
+plt.rcParams['xtick.labelsize'] = 12 
+plt.rcParams['ytick.labelsize'] = 12 
+plt.rcParams['legend.fontsize'] = 12 
+plt.rcParams['figure.titlesize'] = 12 
+plt.rcParams['image.cmap'] = 'jet' 
+plt.rcParams['image.interpolation'] = 'none' 
+plt.rcParams['figure.figsize'] = (10, 10
+                                 ) 
+plt.rcParams['axes.grid']=False
+plt.rcParams['lines.linewidth'] = 2 
+plt.rcParams['lines.markersize'] = 8
+colors = ['xkcd:pale orange', 'xkcd:sea blue', 'xkcd:pale red', 'xkcd:sage green', 'xkcd:terra cotta', 'xkcd:dull purple', 'xkcd:teal', 'xkcd: goldenrod', 'xkcd:cadet blue',
+'xkcd:scarlet']
+
+def get_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
+
+img_2 = get_image('inputimage.jpg')
+plt.imshow(img_2)
+plt.axis('off')
+
+def RGB2HEX(color):
+    return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
+
+number_of_colors = 10
+
+image = img_2
+
+modified_image = image.reshape(image.shape[0]*image.shape[1], 3)
+
+clf = KMeans(n_clusters = number_of_colors)
+labels = clf.fit_predict(modified_image)
+
+counts = Counter(labels)
+
+center_colors = clf.cluster_centers_
+# We get ordered colors by iterating through the keys
+ordered_colors = [center_colors[i] for i in counts.keys()]
+hex_colors = [RGB2HEX(ordered_colors[i]) for i in counts.keys()]
+rgb_colors = [ordered_colors[i] for i in counts.keys()]
+
+plt.title('Colors Detection ($n=10$)', fontsize=20)
+plt.pie(counts.values(), labels = hex_colors, colors = hex_colors)
+
+for i in range(len(rgb_colors)):
+    rgb_colors[i] = rgb_colors[i].astype(int)
+
+def inthreshold(array):
+    count = 0
+    for i in range(len(array)):
+        if array[i]>=-12 and array[i]<=12:
+            count=count+1
+    return count
+
+def show_color(col_index):
+    color = col_index
+    sub_image = (img_2-rgb_colors[color])
+    ZEROS_VALUES = []
+    COUNT = []
+    for i in range(len(sub_image)):
+        for j in range(len(sub_image[i])):
+            e = sub_image[i,j]
+            #print(e.shape)
+            count = inthreshold(e)
+            COUNT.append(count)
+            if count==2:
+                ZEROS_VALUES.append([i,j])
+    color_arr=(np.zeros((16,16,3))+rgb_colors[color]).astype(int)
+    normalized = sub_image - sub_image.min()
+    normalized = ((sub_image/sub_image.max())*255).astype(int)
+    ZEROS_IMAGE = img_2.copy()
+    for i in range(len(ZEROS_VALUES)):
+        ZEROS_IMAGE[ZEROS_VALUES[i][0],ZEROS_VALUES[i][1],:] = [250,250,250]
+    plt.subplot(1,3,1)
+    plt.imshow(ZEROS_IMAGE.astype(int))
+    plt.subplot(1,3,2)
+    plt.imshow(img_2)
+    plt.subplot(1,3,3)
+    #pwargs = {'interpolation':'nearest'}
+    plt.imshow(color_arr)
+
+show_color(4)
+
+def square_maker():
+    inp_img = img_2
+    h = int(img_2.shape[0])
+    step_h = int(h/10) 
+    w = int(img_2.shape[1])
+    step_w = int(w/10) 
+    X = np.arange(0,h+step_h,step_h)
+    Y =np.arange(0,w+step_w,step_w)
+    squares = [inp_img[0:step_h,0:step_w]]
+    for i in range(0,len(X)-1):
+        for j in range(0,len(Y)-1):
+            squares.append(inp_img[X[i]:X[i+1],Y[j]:Y[j+1]])
+    return np.array(squares)[1::]
+
+def color_computing(array):
+    DIFF = []
+    squared_image = square_maker()
+    for square in squared_image:
+        DIFF_COLOR = []
+        for color in range(len(rgb_colors)):
+            diff = np.abs(square - rgb_colors[color])
+            DIFF_COLOR.append(diff.mean())
+        DIFF.append(DIFF_COLOR)
+    return np.array(DIFF)
+    
+def best_color_plot(selected_slice):
+    plt.subplot(1,2,1)
+    plt.title('Retrieved Color')
+    plt.imshow((np.zeros((16,16,3))+ rgb_colors[color_computing(img_2)[selected_slice].argmin()]).astype(int))
+    plt.subplot(1,2,2)
+    plt.title('Selected Square: '+ str(selected_slice))
+    plt.imshow(square_maker()[selected_slice])
+
+
+best_color_plot(5)
+
+best_color_plot(25)
+
+best_color_plot(90)
+
+def build_summary():
+    results = color_computing(img_2)
+    cols = ['Square Number'] + hex_colors
+    sorted_results = pd.DataFrame(columns= cols)
+    k=0
+
+    for r in results:
+        d = {'Square Number':int(k)}
+        for c in range(len(hex_colors)):
+            d[hex_colors[c]] = r[c]*100/r.sum()
+        sorted_results = sorted_results.append(d,ignore_index=True)
+        k=k+1
+    sorted_results['Square Number'] = sorted_results['Square Number'].astype(int)
+    return sorted_results
+
+summary_img_2 = build_summary()
+
+summary_img_2.head()
